62bebc2cc150b3286b9b6887958ac14e1c657a3d
ilunni <jperezvisaires@gmail.com>
2020-01-25 20:01:05 +0100

    Primer intento de implementar resnet-ae. Adivinar porque el output devuelve (50, 50, 3).

---
+++ resnet-ae.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Input, Reshape, Flatten, Dense, UpSampling2D, BatchNormalization, ReLU, Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+# Input shape of the image fed to the ResNet50 network.
+resnet_input_shape = (64, 64, 3)
+resnet_input = Input(shape = resnet_input_shape)
+
+# Create the Resnet50 model with weights pretrained on ImageNet.
+resnet = ResNet50(include_top = False,
+                  weights = "imagenet",
+                  input_tensor = resnet_input,
+                  input_shape = resnet_input_shape)
+
+# resnet.summary()
+
+loss = "mse"
+optimizer = Adam
+
+main_input = resnet_input
+
+x = main_input
+x = resnet(x)
+x = Reshape(target_shape = (8192, ))(x)
+x = Dense(units = 128)(x)
+x = Dense(units = 8192)(x)
+x = Reshape(target_shape = (8, 8, 128))(x)
+x = UpSampling2D(size = (2, 2), data_format = "channels_last", interpolation = "nearest")(x)
+x = Conv2D(filters = 128, kernel_size = (3, 3), strides = (1, 1), data_format = "channels_last")(x)
+x = BatchNormalization()(x)
+x = ReLU()(x)
+x = UpSampling2D(size = (2, 2), data_format = "channels_last", interpolation = "nearest")(x)
+x = Conv2D(filters = 64, kernel_size = (3, 3), strides = (1, 1), data_format = "channels_last")(x)
+x = BatchNormalization()(x)
+x = ReLU()(x)
+x = UpSampling2D(size = (2, 2), data_format = "channels_last", interpolation = "nearest")(x)
+x = Conv2D(filters = 3, kernel_size = (3, 3), strides = (1, 1), data_format = "channels_last")(x)
+x = BatchNormalization()(x)
+x = ReLU()(x)
+x = Activation("sigmoid")(x)
+
+main_output = x
+
+model = Model(inputs = main_input, outputs = main_output)
+
+model.summary()
